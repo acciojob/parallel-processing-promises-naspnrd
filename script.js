ec1dc7740297e55da0370fb7d9de673f68adfb45
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-08-30 20:50:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,30 +10,22 @@ const images = [
  { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-
 function downloadImage(image) {
 	return new Promise((resolve, reject) => {
 		const img = new Image();
-		console.log("img--", img);
-		// new Number()
-		// new String()
-		img.onload = () => resolve(img) // success
-		img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`)) // failure
-		img.src = image.url
+		img.onload = () => resolve(img)
+		img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`))
+		img.src = image.url;
 	})
 }
 
-function displayImages(images){
+function displayImages(images) {
 	images.forEach(img => output.appendChild(img));
 }
-
 btn.addEventListener('click', () => {
-	// const downloadPromises = images.map(downloadImage);
 	const downloadPromises = images.map((image) => downloadImage(image));
-	Promise.all(downloadPromises)
-		.then(displayImages)
-		// .then((images) => displayImages(images))
-		.catch(error => console.log(error)) 
+	Promise.all(downloadPromises).then(displayImages)
+	.catch((error) => console.log(error))
 })
 
 
